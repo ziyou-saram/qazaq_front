0919243963b8373d6e5f1c685703b840b6c0da6c
ziyou-saram <101335207+ziyou-saram@users.noreply.github.com>
2026-02-16 19:47:07 +0500

    auth

--- apps/public/src/middleware.ts
+++ apps/public/src/middleware.ts
@@ -4,6 +4,8 @@ import { routing } from './i18n/routing';
 export default createMiddleware(routing);
 
 export const config = {
-    // Match only internationalized pathnames
-    matcher: ['/', '/(ru|kk|en)/:path*']
+    // Match all pathnames except for
+    // - … if they start with `/api`, `/_next` or `/_vercel`
+    // - … the ones containing a dot (e.g. `favicon.ico`)
+    matcher: ['/((?!api|_next|_vercel|.*\\..*).*)']
 };
